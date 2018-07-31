3e38c8bca7cfdc6048b4a3f732a960ce08a689e6
yhy <18771350464@163.com>
2018-07-31 21:41:41 +0800

    学习 vue 中

---
+++ src/components/Computed.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="example">
+    <p>Original message: "{{ message }}"</p>
+    <p>Computed reversed message: "{{ reversedMessage }}"</p>
+    <span @click="lower($event, 11)">点击</span>
+    <Watchs />
+  </div>
+</template>
+<script>
+import Watchs from './Watch.vue'
+  export default {
+    name: 'child1',
+    components: {
+     Watchs 
+    },
+    data (){
+      return {message: 'abcdef'}
+     },
+    computed: {
+      // 计算属性的 getter
+      reversedMessage: function () {
+        // `this` 指向 vm 实例
+        return this.message.split('').reverse().join('')
+      }
+    },
+    methods: {
+      lower(e, data){
+        console.log(e.target.innerHTML, data)
+      }
+    }
+  }
+</script>
